c88082a3b7977459499b0017c537e7acb615b798
*Ethan <*egmotta88@gmail.com*>
2025-04-05 23:42:22 -0700

    Added my quote and Python script of that quote

---
+++ s24_motta.md
@@ -0,0 +1,6 @@
+"Breathe, breathe in the air, don't be afraid to care."
+"And all you touch and all you see, is all your life will ever be."
+- Pink Floyd, Breathe(In the air), The Dark Side of the Moon
+1973
+
+
---
+++ s24_motta.py
@@ -0,0 +1,4 @@
+print("Breathe, breathe in the air, don't be afraid to care.")
+print("And all you touch and all you see, is all your life will ever be.")
+print("- Pink Floyd, Breathe(In the air), The Dark Side of the Moon")
+print("1973")
